ec7c5824a15cbe4953030cded7082c8b55b6a858
Andreas Hellwig <a5308y@gmail.com>
2019-06-23 07:06:14 +0200

    Refactoring
    - extract DataMuseApiCall
    - python naming conventions

--- synonymizer.py
+++ synonymizer.py
@@ -7,28 +7,30 @@ class SynonymSelectionInputHandler(sublime_plugin.ListInputHandler):
     def __init__(self, view):
         self.view = view
 
-    def name(self):
-        return 'name'
-
-    def itemToWord(self, synonym):
-        return synonym['word']
+    def selected_word(self):
+        return 'selected_word'
 
     def list_items(self):
-        wordRegion = self.view.sel()[0]
-        word = self.view.substr(wordRegion)
-        response = urllib.request.urlopen('https://api.datamuse.com/words?ml=' + word)
-        content = response.read()
-        items = list(map(self.itemToWord, json.loads(content.decode('utf-8'))))
-        return items
+        word_region = self.view.sel()[0]
+        word = self.view.substr(word_region)
+        return DataMuseApiCall().run(word)
 
 
 class SynonymizerCommand(sublime_plugin.TextCommand):
-  def run(self, edit, name):
-    wordRegion = self.view.sel()[0]
-    self.view.replace(edit, wordRegion, name)
+  def run(self, edit, selected_word):
+    word_region = self.view.sel()[0]
+    self.view.replace(edit, word_region, selected_word)
 
   def input(self, args):
-      print(args)
-      if 'name' not in args:
+      if 'selected_word' not in args:
           return SynonymSelectionInputHandler(self.view)
       return None
+
+class DataMuseApiCall():
+  def run(self, word):
+      response = urllib.request.urlopen('https://api.datamuse.com/words?ml=' + word)
+      content = response.read()
+      return list(map(self.item_to_word, json.loads(content.decode('utf-8'))))
+
+  def item_to_word(self, synonym):
+      return synonym['word']
